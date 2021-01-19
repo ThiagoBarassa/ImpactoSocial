bc92363c097c7f21b06f48a3fb2c461039e01db5
ThiagoBarassa <thiagobarassa@gmail.com>
2021-01-19 14:00:44 -0300

    Adicionando components e diricionamento de rotas

--- impactoSocial/src/app/app-routing.module.ts
+++ impactoSocial/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { ForumComponent } from './forum/forum.component';
+import { VideoComponent } from './video/video.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo: 'corpo', pathMatch: 'full'},
+
+  {path:'corpo', component:BodyComponent},
+  {path:'midia',component:VideoComponent},
+  {path:'forum',component:ForumComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- impactoSocial/src/app/app.module.ts
+++ impactoSocial/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { VideoComponent } from './video/video.component';
+import { ForumComponent } from './forum/forum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    VideoComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
